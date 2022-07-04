de3b4574df7d3f016a964a46e248a9fc68be5b59
JAEGER <omer.kayalar0101@icloud.com>
2022-07-04 15:39:14 +0300

    feat: use translations

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import '../styles/globals.css';
 import '../styles/fonts/fonts.css';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
