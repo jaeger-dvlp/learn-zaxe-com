f8d80a6310bbf972429e5a35b2318b5201ec32b4
JAEGER <omer.kayalar0101@icloud.com>
2022-07-05 16:28:26 +0300

    style: pixel design

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
-import Images from '../images/Images';
+import Images from '../../images/Images';
 import LanguageSwitcher from './LanguageSwitcher';
 
 export default function Header() {
@@ -8,7 +8,7 @@ export default function Header() {
     <div className="w-full bg-transparent p-0 flex flex-wrap justify-center items-center absolute left-0 top-0">
       <div className="w-full relative bg-transparent max-w-zaxe flex flex-wrap justify-center items-center p-5">
         {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a href="/" className="w-full max-w-[125px] block">
+        <a href="/" className="w-full max-w-[200px] block">
           <Image
             alt="Zaxe Knowledge Base Logo"
             src={Images.logo.zxkb}
