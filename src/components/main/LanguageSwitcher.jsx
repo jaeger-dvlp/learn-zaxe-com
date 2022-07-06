a66bde709d06295f61039a981e29ef7b24969cbc
JAEGER <omer.kayalar0101@icloud.com>
2022-07-06 17:18:50 +0300

    style: transition duration

--- src/components/main/LanguageSwitcher.jsx
+++ src/components/main/LanguageSwitcher.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { i18n } from 'next-i18next';
 import { useRouter } from 'next/router';
-
 import { FaChevronDown } from 'react-icons/fa';
 
 export default function LanguageSwitcher() {
@@ -12,14 +11,14 @@ export default function LanguageSwitcher() {
   return (
     <div className="absolute group grid grid-cols-1 place-content-start place-items-start min-w-[60px] font-zaxe right-5 top-1/2 -translate-y-1/2 text-[#585858]">
       <div className="w-full grid grid-cols-2 items-center border border-[#585858] px-2 p-0 rounded-sm">
-        <div className="w-full text-sm transition-all duration-150">
+        <div className="w-full text-sm transition-all duration-300">
           {currentLanguage.toUpperCase()}
         </div>
         <div className="flex justify-end w-full">
-          <FaChevronDown className="text-xs transition-all duration-150 group-hover:rotate-180" />
+          <FaChevronDown className="text-xs transition-all duration-300 group-hover:rotate-180" />
         </div>
       </div>
-      <div className="absolute left-0 grid invisible w-full grid-cols-1 gap-1 p-0 pt-2 m-0 transition-all duration-150 -translate-y-4 opacity-0 place-content-start place-items-center group-hover:visible group-hover:opacity-100 group-hover:-translate-y-0 top-full">
+      <div className="absolute left-0 grid invisible w-full grid-cols-1 gap-1 p-0 pt-2 m-0 transition-all duration-300 scale-75 -translate-y-2 opacity-0 place-content-start place-items-center group-hover:visible group-hover:opacity-100 group-hover:-translate-y-0 group-hover:scale-100 top-full">
         {router.locales.map(
           (locale) =>
             locale !== currentLanguage && (
