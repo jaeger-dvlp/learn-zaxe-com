2b158c820a48d008ec6022cb8b9c20b84bbd7dcb
JAEGER <omer.kayalar0101@icloud.com>
2022-07-06 10:25:45 +0300

    feat: create content structure

---
+++ src/content/Content.jsx
@@ -0,0 +1,27 @@
+const Content = {
+  products: [
+    {
+      type: '3d-printer',
+      name: 'Zaxe Z3',
+      slug: 'zaxe-z3',
+      model: 'Z3',
+      image: null,
+    },
+    {
+      type: '3d-printer',
+      name: 'Zaxe X3',
+      slug: 'zaxe-x3',
+      model: 'X3',
+      image: null,
+    },
+    {
+      type: 'software',
+      name: 'Zaxe xDesktop',
+      slug: 'zaxe-xdesktop',
+      model: 'xDesktop',
+      image: null,
+    },
+  ],
+};
+
+export default Content;
