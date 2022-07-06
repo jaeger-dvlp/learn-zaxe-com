d44be8b68681c252aa78a69a129e057a6667cba1
JAEGER <omer.kayalar0101@icloud.com>
2022-07-06 10:26:09 +0300

    export: images

--- src/images/Images.jsx
+++ src/images/Images.jsx
@@ -2,6 +2,17 @@ const Images = {
   logo: {
     zxkb: require('./misc/logo-zxkb.png'),
   },
+  products: {
+    'zaxe-z3': {
+      main: require('./3dprinters/z3.webp'),
+    },
+    'zaxe-x3': {
+      main: require('./3dprinters/x3.webp'),
+    },
+    'zaxe-xdesktop': {
+      main: require('./softwares/xdesktop.webp'),
+    },
+  },
 };
 
 export default Images;
