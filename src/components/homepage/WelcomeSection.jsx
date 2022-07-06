2bdddae81b3b3d7b67cc988fdff25000ed7525b2
JAEGER <omer.kayalar0101@icloud.com>
2022-07-06 09:51:39 +0300

    convert to component

---
+++ src/components/homepage/WelcomeSection.jsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'next-i18next';
+import React from 'react';
+import SearchBar from '../misc/SearchBar';
+
+export default function WelcomeSection() {
+  const { t } = useTranslation();
+  return (
+    <div className="w-full !font-zaxe grid grid-cols-1 place-content-start place-items-center pt-[15vh] bg-white">
+      <div className="grid w-full grid-cols-1 p-5 pb-10 max-w-zaxe pt-7 place-content-start place-items-center">
+        <div className="grid grid-cols-1 gap-0 place-content-center place-items-center">
+          <h1 className="xl:text-6xl lg:text-6xl text-xl text-center font-bold text-[#515151]">
+            {t('homepage.banner.heading')}
+          </h1>
+          <h1 className="text-xl font-bold text-center xl:text-6xl lg:text-6xl text-zaxe">
+            {t('homepage.banner.second-heading')}
+          </h1>
+        </div>
+        <div className="relative grid w-full grid-cols-1 my-5 xl:my-10 lg:my-10 place-content-center place-items-center">
+          <p className="xl:text-lg lg:text-lg text-xs w-full max-w-[550px] text-center font-medium text-[#666666]">
+            {t('homepage.banner.description')}
+          </p>
+        </div>
+        <div className="relative grid w-full grid-cols-1 place-content-center place-items-center">
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  );
+}
