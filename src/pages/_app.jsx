88228ef7834fd525256a88e9041f25dafa296626
JAEGER <omer.kayalar0101@icloud.com>
2022-07-07 09:58:23 +0300

    init: aos

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import '../styles/globals.css';
+import '../styles/animations.css';
 import '../styles/fonts/fonts.css';
 import { appWithTranslation } from 'next-i18next';
 import nextI18nextConfig from '../../next-i18next.config';
 
 function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    AOS.init({ once: true, mirror: false });
+  }, []);
   return <Component {...pageProps} />;
 }
 
