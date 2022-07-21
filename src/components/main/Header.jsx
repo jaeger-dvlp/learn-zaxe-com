3b49213346b327e0e32c83bec3dc2a9f6c85daf4
JAEGER <omer.kayalar0101@icloud.com>
2022-07-21 15:25:33 +0300

    feat: route to homepage with localce

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Images from '../../images/Images';
 import LanguageSwitcher from './LanguageSwitcher';
 
@@ -7,18 +9,16 @@ function Header() {
   return (
     <div className="absolute top-0 left-0 flex flex-wrap items-center justify-center w-full p-0 bg-transparent">
       <div className="relative flex flex-wrap items-center justify-center w-full p-5 bg-transparent max-w-app">
-        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a
-          href="/"
-          className="w-full xl:max-w-[200px] lg:max-w-[200px] max-w-[100px] block"
-        >
-          <Image
-            alt="Zaxe Knowledge Base Logo"
-            src={Images.logo.zxkb}
-            className="w-full h-full transition-all duration-150 hover:scale-90"
-            layout="responsive"
-          />
-        </a>
+        <Link href="/" locale={useRouter().locale}>
+          <a className="w-full xl:max-w-[200px] lg:max-w-[200px] max-w-[100px] block">
+            <Image
+              alt="Zaxe Knowledge Base Logo"
+              src={Images.logo.zxkb}
+              className="w-full h-full transition-all duration-150 hover:scale-90"
+              layout="responsive"
+            />
+          </a>
+        </Link>
         <LanguageSwitcher />
       </div>
     </div>
