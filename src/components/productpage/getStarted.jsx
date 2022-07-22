5de9e2bd82c0bd7054b8a4a92480194ad5ffeebb
JAEGER <omer.kayalar0101@icloud.com>
2022-07-22 10:16:47 +0300

    feat: complete video component without interactivity

--- src/components/productpage/getStarted.jsx
+++ src/components/productpage/getStarted.jsx
@@ -1,10 +1,17 @@
 import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
 import React from 'react';
 import { BsFillPlayFill } from 'react-icons/bs';
 
 function GetStarted({ product }) {
   const { starterVideos } = product.content || null;
   const { t } = useTranslation();
+
+  const playVideo = ({ label, videoURL }) => {
+    // eslint-disable-next-line no-alert
+    alert(`${t(label)} - ${videoURL}`);
+  };
+
   return (
     <div
       data-aos="fade"
@@ -18,14 +25,35 @@ function GetStarted({ product }) {
         </h1>
         <div className="flex flex-wrap items-center justify-center w-full gap-16">
           {starterVideos &&
-            starterVideos.map(({ label }) => (
-              <div className="relative grid w-full max-w-xs grid-cols-1 gap-0 p-0 m-0 group place-content-start place-items-center">
-                <div className="w-full overflow-hidden cursor-pointer relative bg-white border border-zinc-100 rounded-2xl h-[195px] shadow-xl flex justify-center items-center">
-                  <BsFillPlayFill className="absolute group-hover:scale-125 group-hover:text-zaxe transition-all duration-150 text-[#232323] text-6xl text-center -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2" />
+            starterVideos.map(({ label, thumbnail, videoURL }) => (
+              <div
+                key={`${label}Video`}
+                onClick={() => playVideo({ label, videoURL })}
+                onKeyPress={() => playVideo({ label, videoURL })}
+                role="button"
+                tabIndex={0}
+                className="relative grid w-full max-w-xs grid-cols-1 gap-0 p-0 m-0 group place-content-start place-items-center"
+              >
+                <div className="w-full p-0 overflow-hidden cursor-pointer relative bg-white rounded-2xl h-[195px] shadow-xl grid grid-cols-1 place-content-center place-items-center gap-0">
+                  <div className="relative block w-full overflow-hidden z-[1]">
+                    <Image
+                      src={thumbnail}
+                      alt={label}
+                      layout="responsive"
+                      objectFit="contain"
+                      objectPosition="center"
+                      height="100%"
+                      width="100%"
+                    />
+                  </div>
+                  <div className="w-full h-full bg-black/40 group-hover:bg-black/10 transition-all duration-200 absolute left-0 top-0 z-[3]" />
+                  <BsFillPlayFill className="absolute group-hover:scale-125 z-[3] group-hover:text-zaxe transition-all duration-200 text-zinc-200 text-6xl text-center -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2" />
                 </div>
-                <div className="w-full max-w-[80%] group-hover:bg-zaxe transition-all duration-150 bg-[#232323] p-3 rounded-b-2xl text-center grid gridcols-1 place-content-center place-items-center">
-                  <span className="text-center text-white">{t(label)}</span>
-                  <span className="text-xs text-center text-zinc-400 ">
+                <div className="w-full -mt-2 max-w-[80%] group-hover:bg-zaxe transition-all duration-200 bg-[#232323] p-3 pt-4 rounded-b-2xl text-center grid gridcols-1 place-content-center place-items-center">
+                  <span className="text-center transition-all duration-200 text-zinc-200 group-hover:text-white">
+                    {t(label)}
+                  </span>
+                  <span className="text-xs text-center transition-all duration-200 text-zinc-400 group-hover:text-zinc-300">
                     Video
                   </span>
                 </div>
