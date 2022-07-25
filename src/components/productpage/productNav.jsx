f98222a452d2b8b1a08e05fcf380ed3edee692f9
JAEGER <omer.kayalar0101@icloud.com>
2022-07-25 10:32:03 +0300

    beautify

--- src/components/productpage/productNav.jsx
+++ src/components/productpage/productNav.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 import { AiOutlineSearch } from 'react-icons/ai';
-import Link from 'next/link';
 
 function ProductNav({ product }) {
   const { t } = useTranslation();
