bcf5846d3981caf4e4c4fd6d796fb38312a80611
JAEGER <omer.kayalar0101@icloud.com>
2022-07-25 13:56:11 +0300

    feat: create loader component

---
+++ src/components/main/Loader.jsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
+import Images from '../../images/Images';
+
+function Loader() {
+  const Router = useRouter();
+  const [loader, setLoader] = React.useState({
+    isActive: true,
+    inHTML: true,
+  });
+
+  const HideLoader = () => {
+    setTimeout(() => setLoader({ isActive: false, inHTML: true }), 1000);
+    setTimeout(() => setLoader({ isActive: false, inHTML: false }), 1500);
+  };
+
+  const StartLoader = () => {
+    setLoader({
+      isActive: true,
+      inHTML: true,
+    });
+    HideLoader();
+  };
+
+  useEffect(() => StartLoader(), [Router.asPath]);
+
+  return (
+    loader.inHTML && (
+      <div
+        className={`${
+          loader.isActive
+            ? 'transition-none duration-[0ms] !opacity-100 !visible'
+            : 'transition-all duration-500 !opacity-0 !invisible'
+        } zaxe-app-loader fixed !z-[99] top-0 left-0 grid w-screen h-screen grid-cols-1 p-5 
+		bg-white place-content-center place-items-center`}
+      >
+        <div
+          data-aos="zoom-out"
+          data-aos-delay={200}
+          data-aos-duration={500}
+          className="w-full max-w-[5rem] p-0 m-0"
+        >
+          <Image src={Images.loader.zxkb} alt="Zaxe Knowledge Base" />
+        </div>
+      </div>
+    )
+  );
+}
+
+export default Loader;
