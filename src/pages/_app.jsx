89a895f0735ddbbaab46284555e687c2f975c1fa
JAEGER <omer.kayalar0101@icloud.com>
2022-07-28 15:10:14 +0300

    feat: wrap app with AppWrapper

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,12 +6,19 @@ import '../styles/animations.css';
 import '../styles/fonts/fonts.css';
 import { appWithTranslation } from 'next-i18next';
 import nextI18nextConfig from '../../next-i18next.config';
+import AppWrapper from '../components/contexts/AppContext';
+import NotificationPopup from '../components/main/NotificationPopup';
 
 function MyApp({ Component, pageProps }) {
   React.useEffect(() => {
     AOS.init({ once: true, mirror: false });
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <AppWrapper>
+      <NotificationPopup />
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
 
 export default appWithTranslation(MyApp, nextI18nextConfig);
