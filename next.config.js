9aded5a8dbb3398f06249b4f476e1d1deb124541
JAEGER <omer.kayalar0101@icloud.com>
2022-08-02 18:11:08 +0300

    init import alias

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
+const path = require('path');
+const { i18n } = require('@/next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  webpack: (config) => {
+    const newConf = config;
+    newConf.plugins = config.plugins || [];
+    newConf.optimization.providedExports = true;
+    newConf.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return newConf;
+  },
 };
 
 module.exports = nextConfig;
