f708536717083167e65e73e71fce38d40e55d152
JAEGER <omer.kayalar0101@icloud.com>
2022-08-02 17:10:47 +0300

    beautify

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import AOS from 'aos';
+import React from 'react';
 import 'aos/dist/aos.css';
 import '../styles/globals.css';
 import '../styles/animations.css';
 import '../styles/fonts/fonts.css';
 import { appWithTranslation } from 'next-i18next';
+
+import Loader from '../components/main/Loader';
+import Header from '../components/main/Header';
 import nextI18nextConfig from '../../next-i18next.config';
 import AppWrapper from '../components/contexts/AppContext';
-import Header from '../components/main/Header';
-import Loader from '../components/main/Loader';
 import NotificationPopup from '../components/main/NotificationPopup';
 
 function MyApp({ Component, pageProps }) {
