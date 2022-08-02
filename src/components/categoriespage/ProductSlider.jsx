4d97347ce641563ee2d7730384338b35946627de
JAEGER <omer.kayalar0101@icloud.com>
2022-08-02 16:49:15 +0300

    feat: create component

---
+++ src/components/categoriespage/ProductSlider.jsx
@@ -0,0 +1,103 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { i18n } from 'next-i18next';
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import Content from '../../content/Content';
+
+function ProductSlider() {
+  const [products] = React.useState([
+    Content.products.find(({ slug }) => slug === 'zaxe-xdesktop'),
+    Content.products.find(({ slug }) => slug === 'zaxe-z3'),
+    Content.products.find(({ slug }) => slug === 'zaxe-x3'),
+  ]);
+
+  const [activeSlide, setActiveSlide] = React.useState(null);
+
+  return (
+    <div className="w-full">
+      <Swiper
+        className="relative z-[1] grid grid-cols-1 place-content-center
+        place-items-center items-center max-w-xl w-full gap-0 !overflow-hidden"
+        onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        modules={[Navigation]}
+        slidesPerView={3}
+        initialSlide={1}
+        spaceBetween={0}
+        centeredSlides
+      >
+        <button
+          type="button"
+          className="absolute z-[5] disabled:opacity-50 left-0 top-1/2 -translate-y-1/2
+          swiper-button-prev active:bg-zinc-300 transition-all duration-200 active:scale-75 rounded-xl"
+          disabled={activeSlide === 0}
+        >
+          <FiChevronLeft className="text-4xl" />
+        </button>
+        <button
+          type="button"
+          className="absolute z-[5] disabled:opacity-50  right-0 top-1/2 -translate-y-1/2
+          swiper-button-next active:bg-zinc-300 transition-all duration-200 active:scale-75 rounded-xl"
+          disabled={activeSlide === products.length - 1}
+        >
+          <FiChevronRight className="text-4xl" />
+        </button>
+        {products.map((product, index) => (
+          <SwiperSlide
+            key={`route-product-slide-element-${product.slug}`}
+            className={`${
+              index === activeSlide ? 'z-[2]' : 'z-[1]'
+            } relative py-[3.5rem]`}
+          >
+            <Link
+              href={`/products/${product.category.slug}/${product.slug}`}
+              locale={i18n.language}
+            >
+              <a
+                className={`w-full ${
+                  index === activeSlide
+                    ? 'scale-[1.8] opacity-100'
+                    : 'scale-[1] hover:opacity-70 opacity-30'
+                } grid grid-cols-1 relative text-[#2C2C2C] place-content-center
+                place-items-center h-full px-1 py-5 transition-all
+                duration-300 rounded-3xl gap-2`}
+              >
+                <div
+                  className={`w-full max-w-[360px]
+                  ${
+                    product.slug === 'zaxe-xdesktop' &&
+                    'max-w-[80px] sm:max-w-[100px] md:max-w-[130px]'
+                  }`}
+                >
+                  <Image
+                    src={product.images.main.img}
+                    alt={product.images.main.alt}
+                    layout="responsive"
+                  />
+                </div>
+                <h1
+                  className={`text-xs font-bold text-current opacity-0
+                  transition-all duration-200
+                  ${index === activeSlide && '!opacity-100'}`}
+                >
+                  {product.model}
+                </h1>
+              </a>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+export default ProductSlider;
---
+++ src/components/categoriespage/SearchBar.jsx
@@ -0,0 +1,79 @@
+import { useTranslation } from 'next-i18next';
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+function SearchBar() {
+  const { t } = useTranslation();
+  const [searchBarPHs, setSearchBarPHs] = React.useState({
+    placeHolders: [
+      t('searchbar.placeholders.0'),
+      t('searchbar.placeholders.1'),
+      t('searchbar.placeholders.2'),
+      t('searchbar.placeholders.3'),
+      t('searchbar.placeholders.4'),
+      t('searchbar.placeholders.5'),
+    ],
+    active: 0,
+  });
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setSearchBarPHs({
+        ...searchBarPHs,
+        active:
+          searchBarPHs.placeHolders.length - 1 === searchBarPHs.active
+            ? 0
+            : searchBarPHs.active + 1,
+      });
+    }, 1500);
+    return () => clearInterval(interval);
+  }, [searchBarPHs]);
+
+  React.useEffect(() => {
+    const barForm = document.querySelector('form.main-search-bar');
+    const HandleSearch = (e) => {
+      e.preventDefault();
+      const searchInput = barForm.querySelector('input');
+      // eslint-disable-next-line no-alert
+      alert(
+        searchInput.value
+          ? `${t('searchbar.searching-for')} ${searchInput.value}`
+          : `${t('searchbar.searching-for')} Nothing`
+      );
+    };
+
+    barForm.addEventListener('submit', HandleSearch);
+
+    return () => barForm && barForm.removeEventListener('submit', HandleSearch);
+  }, []);
+  return (
+    <div className="grid w-full grid-cols-1 place-content-start place-items-center gap-7">
+      <h1 className="xl:text-4xl lg:text-4xl text-xl font-bold text-[#585858] text-center">
+        What can we help you with?
+      </h1>
+      <form
+        className="relative flex items-center justify-center xl:min-h-[80px] lg:min-h-[80px]
+    min-h-[60px] w-full xl:max-w-xl lg:max-w-xl max-w-sm overflow-hidden transition-all
+    duration-150 rounded-[20px] main-search-bar ring-2 ring-transparent focus-within:ring-zaxe
+    xl:h-12 lg:h-12 h-9"
+      >
+        <input
+          placeholder={searchBarPHs.placeHolders[searchBarPHs.active]}
+          className=" bg-[#F5F5F5] xl:text-lg lg:text-lg text-sm placeholder-slate-300
+        outline-none p-1 px-4 pr-0 relative w-full h-full text-center"
+          type="text"
+        />
+        <button
+          type="submit"
+          className="w-14 bg-[#F5F5F5] transition-all duration-100 hover:bg-zinc-200
+        active:bg-zaxe active:text-white text-zaxe h-full p-0 m-0 flex flex-wrap
+        justify-center items-center"
+        >
+          <AiOutlineSearch className="text-2xl" />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
