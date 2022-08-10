814b6dbfb09e990f18e148e6cd1d704ed4bc98e4
JAEGER <omer.kayalar0101@icloud.com>
2022-08-10 14:16:30 +0300

    feat: list categories of product

--- src/pages/products/[category]/[product]/categories/index.jsx
+++ src/pages/products/[category]/[product]/categories/index.jsx
@@ -12,8 +12,13 @@ import SearchBar from '@/src/components/categoriespage/SearchBar';
 import ProductSlider from '@/src/components/categoriespage/ProductSlider';
 
 function Categories({ product: stringProduct }) {
-  const [product] = React.useState(JSON.parse(stringProduct));
-  const { category, slug } = product;
+  const product = JSON.parse(stringProduct);
+  const {
+    category: productCategory,
+    content: productContent,
+    slug: productSlug,
+  } = product;
+  const { categories: postCategories } = productContent;
   const { t } = useTranslation();
 
   return (
@@ -21,13 +26,31 @@ function Categories({ product: stringProduct }) {
       <>
         <Head>
           <title>
-            {t(`productmeta:${category.slug}.${slug}.categories-title`)}
+            {t(
+              `productmeta:${productCategory.slug}.${productSlug}.categories-title`
+            )}
           </title>
         </Head>
         <div className="grid font-zaxe w-full grid-cols-1 place-content-start place-items-center pt-[20vh]">
           <div className="grid w-full grid-cols-1 p-5 gap-14 max-w-app place-content-start place-items-center">
             <SearchBar />
             <ProductSlider />
+          </div>
+          <div className="flex items-start justify-center w-full gap-10 max-w-app">
+            <div className="grid w-full max-w-xs grid-cols-1 place-content-start place-items-start">
+              <ul>
+                {postCategories.map(
+                  ({ slug: prodCategorySlug, label: prodCategoryLabel }) => (
+                    <li key={`${productSlug}-${prodCategorySlug}`}>
+                      {t(prodCategoryLabel)}
+                    </li>
+                  )
+                )}
+              </ul>
+            </div>
+            <div className="grid w-full grid-cols-1 p-10 xl:grid-cols-3 lg:grid-cols-2 place-content-start place-items-start bg-zinc-100 rounded-xl">
+              <span className="text-zinc-300">Content to go..</span>
+            </div>
           </div>
         </div>
       </>
