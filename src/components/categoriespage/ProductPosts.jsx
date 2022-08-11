781f60e12e450c28f466570df02c1ea908a85dbb
JAEGER <omer.kayalar0101@icloud.com>
2022-08-11 14:30:12 +0300

    feat: Posts filter

--- src/components/categoriespage/ProductPosts.jsx
+++ src/components/categoriespage/ProductPosts.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import Image from 'next/image';
 import { i18n, useTranslation } from 'next-i18next';
 import { BiLinkExternal } from 'react-icons/bi';
+import { useRouter } from 'next/router';
 
 function ProductPosts({ product }) {
+  const router = useRouter();
   const {
     slug: productSlug,
     content: { posts: Posts, categories: Categories },
   } = product;
+  const {
+    query: { c: queryCategory },
+  } = router;
   const { t } = useTranslation();
 
   return (
@@ -18,38 +24,74 @@ function ProductPosts({ product }) {
             category: postCategory,
             title: postTitle,
             title: { en: postKey },
-            thumbnail: postThumbnailt,
-          }) => (
-            <div
-              key={`${postCategory}-${postKey}-${productSlug}`}
-              className="relative grid w-full h-full max-w-xs grid-cols-1 overflow-hidden transition-all duration-200 bg-white shadow-xl cursor-pointer hover:-translate-y-3 hover:shadow-2xl place-content-start place-items-center rounded-xl"
-            >
-              <BiLinkExternal className="absolute z-[3] text-4xl bottom-2 right-2 text-zinc-200" />
-              <div className="relative z-[2] w-full h-[10rem] max-w-xs overflow-hidden rounded-xl">
-                <Image
-                  src={postThumbnailt}
-                  layout="fill"
-                  alt={postTitle.en}
-                  className="object-cover object-center w-full h-full"
+            thumbnail: postThumbnail,
+          }) =>
+            queryCategory ? (
+              postCategory === queryCategory ? (
+                <PostBlock
+                  props={{
+                    postCategory,
+                    postTitle,
+                    postKey,
+                    postThumbnail,
+                    productSlug,
+                    t,
+                    Categories,
+                  }}
                 />
-              </div>
-              <div className="relative z-[1] flex items-end justify-between w-full p-3 bg-white">
-                <div className="grid w-[80%] gap-1 grid-cols-1 place-content-end place-items-start">
-                  <span className="text-xs text-[#868686]">
-                    {t(
-                      Categories.find(({ slug }) => slug === postCategory).label
-                    ) || null}
-                  </span>
-                  <h1 className="text-sm text-[#555555]">
-                    {postTitle[i18n.language]}
-                  </h1>
-                </div>
-              </div>
-            </div>
-          )
+              ) : null
+            ) : (
+              <PostBlock
+                props={{
+                  postCategory,
+                  postTitle,
+                  postKey,
+                  postThumbnail,
+                  productSlug,
+                  t,
+                  Categories,
+                }}
+              />
+            )
         )}
       </div>
     )
+  );
+}
+function PostBlock({ props }) {
+  const {
+    postCategory,
+    postTitle,
+    postKey,
+    postThumbnail,
+    productSlug,
+    t,
+    Categories,
+  } = props;
+  return (
+    <div
+      key={`${postCategory}-${postKey}-${productSlug}`}
+      className="relative grid w-full h-full max-w-xs grid-cols-1 overflow-hidden transition-all duration-200 bg-white shadow-xl cursor-pointer hover:-translate-y-3 hover:shadow-2xl place-content-start place-items-center rounded-xl"
+    >
+      <BiLinkExternal className="absolute z-[3] text-4xl bottom-2 right-2 text-zinc-200" />
+      <div className="relative z-[2] w-full h-[10rem] max-w-xs overflow-hidden rounded-xl">
+        <Image
+          src={postThumbnail}
+          layout="fill"
+          alt={postTitle.en}
+          className="object-cover object-center w-full h-full"
+        />
+      </div>
+      <div className="relative z-[1] flex items-end justify-between w-full p-3 bg-white">
+        <div className="grid w-[80%] gap-1 grid-cols-1 place-content-end place-items-start">
+          <span className="text-xs text-[#868686]">
+            {t(Categories.find(({ slug }) => slug === postCategory).label) ||
+              null}
+          </span>
+          <h1 className="text-sm text-[#555555]">{postTitle[i18n.language]}</h1>
+        </div>
+      </div>
+    </div>
   );
 }
 
