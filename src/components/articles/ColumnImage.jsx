4b4f535e88ec3e1146981305662c55485d85091f
JAEGER <omer.kayalar0101@icloud.com>
2022-08-15 16:50:15 +0300

    feat: Create component

---
+++ src/components/articles/ColumnImage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+
+function ColumnImage({ children, image }) {
+  return (
+    <section className="flex flex-wrap-reverse w-full gap-10 my-5 post-column xl:flex-nowrap lg:flex-nowrap">
+      <section className="w-full xl:max-w-[40%] lg:max-w-[40%] max-w-full">
+        {children}
+      </section>
+      <section className="relative w-full overflow-hidden shadow-xl rounded-xl min-h-[20rem]">
+        <Image
+          src={image}
+          layout="fill"
+          className="object-cover object-center w-full h-full"
+        />
+      </section>
+    </section>
+  );
+}
+
+export default ColumnImage;
