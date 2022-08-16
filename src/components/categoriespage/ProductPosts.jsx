a1cf9f848b4e7d1bf5b72a160976398a3dce74ba
JAEGER <omer.kayalar0101@icloud.com>
2022-08-16 09:13:55 +0300

    feat: route to article

--- src/components/categoriespage/ProductPosts.jsx
+++ src/components/categoriespage/ProductPosts.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { i18n, useTranslation } from 'next-i18next';
 import { BiLinkExternal } from 'react-icons/bi';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function ProductPosts({ product }) {
   const router = useRouter();
@@ -12,7 +13,12 @@ function ProductPosts({ product }) {
     content: { posts: Posts, categories: Categories },
   } = product;
   const {
-    query: { c: queryCategory },
+    query: {
+      product: queryProduct,
+      category: queryPCategory,
+      c: queryCategory,
+    },
+    locale: lang,
   } = router;
   const { t } = useTranslation();
 
@@ -25,17 +31,22 @@ function ProductPosts({ product }) {
             title: postTitle,
             title: { en: postKey },
             thumbnail: postThumbnail,
+            slug: postSlug,
           }) =>
             queryCategory ? (
               postCategory === queryCategory ? (
                 <PostBlock
                   key={`${postCategory}-${postKey}-${productSlug}`}
                   props={{
+                    lang,
+                    postSlug,
                     postCategory,
                     postTitle,
                     postKey,
                     postThumbnail,
                     productSlug,
+                    queryProduct,
+                    queryPCategory,
                     t,
                     Categories,
                   }}
@@ -45,11 +56,15 @@ function ProductPosts({ product }) {
               <PostBlock
                 key={`${postCategory}-${postKey}-${productSlug}`}
                 props={{
+                  lang,
+                  postSlug,
                   postCategory,
                   postTitle,
                   postKey,
                   postThumbnail,
                   productSlug,
+                  queryProduct,
+                  queryPCategory,
                   t,
                   Categories,
                 }}
@@ -62,36 +77,45 @@ function ProductPosts({ product }) {
 }
 function PostBlock({ props }) {
   const {
+    lang,
+    postSlug,
     postCategory,
     postTitle,
-
     postThumbnail,
-
+    queryProduct,
+    queryPCategory,
     t,
     Categories,
   } = props;
   return (
-    <div className="relative grid w-full h-full max-w-xs grid-cols-1 overflow-hidden transition-all duration-200 bg-white shadow-xl cursor-pointer hover:-translate-y-3 hover:shadow-2xl place-content-start place-items-center rounded-xl">
-      <BiLinkExternal className="absolute z-[3] text-4xl bottom-2 right-2 text-zinc-200" />
-      <div className="relative z-[2] w-full h-[10rem] max-w-xs overflow-hidden rounded-xl">
-        <Image
-          placeholder="blur"
-          src={postThumbnail}
-          layout="fill"
-          alt={postTitle.en}
-          className="object-cover object-center w-full h-full"
-        />
-      </div>
-      <div className="relative z-[1] flex items-end justify-between w-full p-3 bg-white">
-        <div className="grid w-[80%] gap-1 grid-cols-1 place-content-end place-items-start">
-          <span className="text-xs text-[#868686]">
-            {t(Categories.find(({ slug }) => slug === postCategory).label) ||
-              null}
-          </span>
-          <h1 className="text-sm text-[#555555]">{postTitle[i18n.language]}</h1>
+    <Link
+      href={`/products/${queryPCategory}/${queryProduct}/article/${postSlug}`}
+      locale={lang}
+    >
+      <a className="relative grid w-full h-full max-w-xs grid-cols-1 overflow-hidden transition-all duration-200 bg-white shadow-xl cursor-pointer hover:-translate-y-3 hover:shadow-2xl place-content-start place-items-center rounded-xl">
+        <BiLinkExternal className="absolute z-[3] text-4xl bottom-2 right-2 text-zinc-200" />
+        <div className="relative z-[2] w-full h-[10rem] max-w-xs overflow-hidden rounded-xl">
+          <Image
+            placeholder="blur"
+            src={postThumbnail}
+            layout="fill"
+            alt={postTitle.en}
+            className="object-cover object-center w-full h-full"
+          />
         </div>
-      </div>
-    </div>
+        <div className="relative z-[1] flex items-end justify-between w-full p-3 bg-white">
+          <div className="grid w-[80%] gap-1 grid-cols-1 place-content-end place-items-start">
+            <span className="text-xs text-[#868686]">
+              {t(Categories.find(({ slug }) => slug === postCategory).label) ||
+                null}
+            </span>
+            <h1 className="text-sm text-[#555555]">
+              {postTitle[i18n.language]}
+            </h1>
+          </div>
+        </div>
+      </a>
+    </Link>
   );
 }
 
