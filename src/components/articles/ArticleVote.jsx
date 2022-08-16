39f440de4af1720d359f1830f105e114d520097e
JAEGER <omer.kayalar0101@icloud.com>
2022-08-16 12:11:01 +0300

    get translated texts

--- src/components/articles/ArticleVote.jsx
+++ src/components/articles/ArticleVote.jsx
@@ -1,8 +1,10 @@
+import { useTranslation } from 'next-i18next';
 import React from 'react';
 import { BsCheckLg } from 'react-icons/bs';
 
 function ArticleVote() {
   const [demoFeedback, setDemoFeedback] = React.useState(false);
+  const { t } = useTranslation();
   return (
     <div className="relative grid w-full grid-cols-1 gap-0 my-20 mt-10 place-content-start place-items-center">
       <div
@@ -13,7 +15,7 @@ function ArticleVote() {
         } transition-all duration-500 grid grid-cols-1 place-content-start place-items-center gap-10`}
       >
         <h1 className="text-2xl font-semibold text-zaxe">
-          Was this article helpful?
+          {t('common:article.vote-area.header')}
         </h1>
         <div className="flex items-center justify-center w-full gap-7">
           <button
@@ -21,14 +23,14 @@ function ArticleVote() {
             onClick={() => setDemoFeedback(true)}
             className="py-1 hover:bg-zaxe hover:-translate-y-1 active:scale-75 hover:border-zaxe hover:text-white transition-all duration-200 px-4 min-w-[6rem] border border-zinc-100 shadow-xl shadow-black/20 rounded-2xl text-zinc-500"
           >
-            Yes
+            {t('common:article.vote-area.vote-button-yes')}
           </button>
           <button
             type="button"
             onClick={() => setDemoFeedback(true)}
             className="py-1 hover:bg-zaxe hover:-translate-y-1 active:scale-75 hover:border-zaxe hover:text-white transition-all duration-200 px-4 min-w-[6rem] border border-zinc-100 shadow-xl shadow-black/20 rounded-2xl text-zinc-500"
           >
-            No
+            {t('common:article.vote-area.vote-button-no')}
           </button>
         </div>
       </div>
@@ -41,7 +43,7 @@ function ArticleVote() {
       >
         <BsCheckLg className="text-lg text-zaxe" />
         <h1 className="text-lg font-semibold text-center text-zaxe">
-          Thank you for your feedback.
+          {t('common:article.vote-area.vote-message-thanks')}
         </h1>
       </div>
     </div>
