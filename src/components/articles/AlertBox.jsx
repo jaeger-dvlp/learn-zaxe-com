074de648f80195f3a642ca26aa62608241eea638
JAEGER <omer.kayalar0101@icloud.com>
2022-08-16 17:21:05 +0300

    feat: add ping circles to alert box

--- src/components/articles/AlertBox.jsx
+++ src/components/articles/AlertBox.jsx
@@ -17,11 +17,17 @@ function AlertBox({ children, alertType }) {
       className={`flex xl:flex-nowrap lg:flex-nowrap flex-wrap border rounded-lg items-center justify-between w-full gap-3 p-3 relative top-5 ${AlertClass}`}
     >
       <span className="flex items-center justify-center w-full xl:w-20 lg:w-20">
-        <IoWarningOutline className="text-5xl text-center text-current" />
+        <IoWarningOutline className="text-5xl text-center text-current relative z-[2]" />
       </span>
       <section className="w-full p-1 py-3 text-xs text-left text-current alert-text">
         {children}
       </section>
+      <span className="absolute top-0 left-0 flex w-4 h-4 -translate-x-1/2 -translate-y-1/2">
+        <span className="absolute inline-flex w-full h-full bg-current rounded-full opacity-75 animate-ping" />
+        <span className="relative flex items-center justify-center w-4 h-4 transition-all bg-white border border-current rounded-full">
+          <span className="w-2 h-2 bg-current rounded-full" />
+        </span>
+      </span>
     </section>
   );
 }
