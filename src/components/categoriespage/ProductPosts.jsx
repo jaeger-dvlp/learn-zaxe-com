18289c77bec6bece6f3787c3ed828026646a6850
JAEGER <omer.kayalar0101@icloud.com>
2022-08-17 11:08:10 +0300

    def pros

--- src/components/categoriespage/ProductPosts.jsx
+++ src/components/categoriespage/ProductPosts.jsx
@@ -1,10 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react';
-import Image from 'next/image';
-import { i18n, useTranslation } from 'next-i18next';
-import { BiLinkExternal } from 'react-icons/bi';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+import PostBlock from '@/src/components/misc/PostBlock';
 
 function ProductPosts({ product }) {
   const router = useRouter();
@@ -18,9 +15,7 @@ function ProductPosts({ product }) {
       category: queryPCategory,
       c: queryCategory,
     },
-    locale: lang,
   } = router;
-  const { t } = useTranslation();
 
   return (
     product && (
@@ -38,16 +33,12 @@ function ProductPosts({ product }) {
                 <PostBlock
                   key={`${postCategory}-${postKey}-${productSlug}`}
                   props={{
-                    lang,
                     postSlug,
                     postCategory,
                     postTitle,
-                    postKey,
                     postThumbnail,
-                    productSlug,
                     queryProduct,
                     queryPCategory,
-                    t,
                     Categories,
                   }}
                 />
@@ -56,16 +47,12 @@ function ProductPosts({ product }) {
               <PostBlock
                 key={`${postCategory}-${postKey}-${productSlug}`}
                 props={{
-                  lang,
                   postSlug,
                   postCategory,
                   postTitle,
-                  postKey,
                   postThumbnail,
-                  productSlug,
                   queryProduct,
                   queryPCategory,
-                  t,
                   Categories,
                 }}
               />
@@ -73,49 +60,6 @@ function ProductPosts({ product }) {
         )}
       </div>
     )
-  );
-}
-function PostBlock({ props }) {
-  const {
-    lang,
-    postSlug,
-    postCategory,
-    postTitle,
-    postThumbnail,
-    queryProduct,
-    queryPCategory,
-    t,
-    Categories,
-  } = props;
-  return (
-    <Link
-      href={`/products/${queryPCategory}/${queryProduct}/article/${postSlug}`}
-      locale={lang}
-    >
-      <a className="relative grid w-full h-full max-w-xs grid-cols-1 overflow-hidden transition-all duration-200 bg-white shadow-xl cursor-pointer hover:-translate-y-3 hover:shadow-2xl place-content-start place-items-center rounded-xl">
-        <BiLinkExternal className="absolute z-[3] text-4xl bottom-2 right-2 text-zinc-200" />
-        <div className="relative z-[2] w-full h-[10rem] max-w-xs overflow-hidden rounded-xl">
-          <Image
-            placeholder="blur"
-            src={postThumbnail}
-            layout="fill"
-            alt={postTitle.en}
-            className="object-cover object-center w-full h-full"
-          />
-        </div>
-        <div className="relative z-[1] flex items-end justify-between w-full p-3 bg-white">
-          <div className="grid w-[80%] gap-1 grid-cols-1 place-content-end place-items-start">
-            <span className="text-xs text-[#868686]">
-              {t(Categories.find(({ slug }) => slug === postCategory).label) ||
-                null}
-            </span>
-            <h1 className="text-sm text-[#555555]">
-              {postTitle[i18n.language]}
-            </h1>
-          </div>
-        </div>
-      </a>
-    </Link>
   );
 }
 
