ab86327cd0e5ed9152e91b72517527f1503775e9
JAEGER <omer.kayalar0101@icloud.com>
2022-08-18 12:02:49 +0300

    set CDN URL configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const { CDNURL_NEXTCONFIG, CDNURL } = process.env;
 const path = require('path');
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  env: {
+    CDNURL,
+  },
+  images: {
+    domains: [CDNURL_NEXTCONFIG],
+  },
   webpack: (config) => {
     const newConf = config;
     newConf.plugins = config.plugins || [];
