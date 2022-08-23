590a5dab20fa3f85bdca7273effb76f6fa9979ee
JAEGER <omer.kayalar0101@icloud.com>
2022-08-23 14:50:05 +0300

    feat: get all posts with details

--- src/utils/postHandler.jsx
+++ src/utils/postHandler.jsx
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { v4 } from 'uuid';
 import matter from 'gray-matter';
 import Content from '../content/Content';
 
@@ -18,26 +19,46 @@ const getPost = async ({ postSlug, productSlug, locale }) => {
 
 const getAllPosts = async ({ locale }) => {
   const Products = Content.products;
-  const allPosts = Promise.all(
-    Products.map(async ({ slug: productSlug, content: { posts } }) =>
-      Promise.all(
-        posts.map(async ({ slug: postSlug }) => {
-          const { data, content } = matter(
-            fs.readFileSync(
-              path.join(`src/posts/${productSlug}/${locale}/${postSlug}.mdx`),
-              'utf8'
-            )
-          );
-          return {
-            data,
-            content,
-          };
-        })
-      )
-    )
+  const allPostsUnfiltered = await Promise.all(
+    Products.map(
+      async ({
+        slug: productSlug,
+        content: { posts },
+        category: { slug: productCategory },
+      }) =>
+        Promise.all(
+          posts
+            .map(async ({ slug: postSlug, thumbnail }) => {
+              const { data } = matter(
+                fs.readFileSync(
+                  path.join(
+                    `src/posts/${productSlug}/${locale}/${postSlug}.mdx`
+                  ),
+                  'utf8'
+                )
+              );
+              return {
+                data: {
+                  ...data,
+                  slug: postSlug,
+                  id: v4().replace(/-/g, ''),
+                  url: `/${locale}/products/${productCategory}/${productSlug}/article/${postSlug}`,
+                  thumbnail,
+                  'product-slug': productSlug,
+                  'product-category': productCategory,
+                },
+              };
+            })
+            .flat()
+        )
+    ).flat()
   );
 
-  return allPosts;
+  const AllPosts = allPostsUnfiltered
+    .map((posts) => posts.map((postF) => postF.data))
+    .flat();
+
+  return AllPosts;
 };
 
 export default { getPost, getAllPosts };
