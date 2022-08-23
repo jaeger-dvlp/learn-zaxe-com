be2c22f78fd9f55a5fc57f10843eef0b2d1be33f
JAEGER <omer.kayalar0101@icloud.com>
2022-08-23 11:07:33 +0300

    feat: get All Posts

--- src/utils/postHandler.jsx
+++ src/utils/postHandler.jsx
@@ -1,17 +1,43 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import Content from '../content/Content';
 
 const getPost = async ({ postSlug, productSlug, locale }) => {
-  const fileContents = fs.readFileSync(
-    path.join(`src/posts/${productSlug}/${locale}/${postSlug}.mdx`),
-    'utf8'
+  const { data, content } = matter(
+    fs.readFileSync(
+      path.join(`src/posts/${productSlug}/${locale}/${postSlug}.mdx`),
+      'utf8'
+    )
   );
-  const { data, content } = matter(fileContents);
   return {
     data,
     content,
   };
 };
 
-export default { getPost };
+const getAllPosts = async ({ locale }) => {
+  const Products = Content.products;
+  const allPosts = Promise.all(
+    Products.map(async ({ slug: productSlug, content: { posts } }) =>
+      Promise.all(
+        posts.map(async ({ slug: postSlug }) => {
+          const { data, content } = matter(
+            fs.readFileSync(
+              path.join(`src/posts/${productSlug}/${locale}/${postSlug}.mdx`),
+              'utf8'
+            )
+          );
+          return {
+            data,
+            content,
+          };
+        })
+      )
+    )
+  );
+
+  return allPosts;
+};
+
+export default { getPost, getAllPosts };
