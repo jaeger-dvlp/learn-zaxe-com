0da81788818028466ee1feeca7ae23d535e3c1e2
JAEGER <omer.kayalar0101@icloud.com>
2022-08-24 14:23:02 +0300

    set meta data

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Content from '@/src/content/Content';
 import { BiInfoCircle } from 'react-icons/bi';
@@ -11,6 +12,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { BsFillFileEarmarkArrowDownFill } from 'react-icons/bs';
 import { MdArticle, MdVideoLibrary } from 'react-icons/md';
 import VideoBlock from '../components/misc/VideoBlock';
+import Images from '../images/Images';
 
 function Search({ posts: Posts }) {
   const router = useRouter();
@@ -109,75 +111,125 @@ function Search({ posts: Posts }) {
   }, [searchQuery, router]);
 
   return (
-    <section className="pt-[20vh] font-zaxe w-full flex flex-wrap justify-center items-start">
-      <section className="grid w-full grid-cols-1 p-5 gap-14 max-w-app place-content-start place-items-center">
-        <SearchBar value={searchQuery || null} />
-        <section className="grid w-full grid-cols-1 pb-10 gap-7 place-content-start place-items-center">
-          <h2 className="flex items-center justify-center gap-2 p-1 px-4 text-xl font-semibold text-center text-white shadow-xl shadow-black/30 rounded-xl bg-zaxe">
-            <MdArticle />
-            <span>{t('common:search.articles')}</span>
-          </h2>
-          <nav className="relative flex flex-wrap items-center justify-center w-full gap-14">
-            {filteredPosts.length > 0 ? (
-              filteredPosts.map((post) => (
-                <PostBlock
-                  key={`post-${post.id}`}
-                  props={{
-                    postSlug: post.slug,
-                    postCategory: post['category-slug'],
-                    postTitle: { [router.locale]: post.title },
-                    postThumbnail: post.thumbnail,
-                    queryProduct: post['product-slug'],
-                    queryPCategory: post['product-category'],
-                    Categories: Content.products.find(
-                      ({ slug }) => slug === post['product-slug']
-                    ).content.categories,
-                  }}
-                />
-              ))
-            ) : (
-              <NoContent />
-            )}
-          </nav>
-        </section>
-        <section className="grid w-full grid-cols-1 pb-10 gap-7 place-content-start place-items-center">
-          <h2 className="flex items-center justify-center gap-2 p-1 px-4 text-xl font-semibold text-center text-white shadow-xl shadow-black/30 rounded-xl bg-zaxe">
-            <MdVideoLibrary />
-            <span>{t('common:search.videos')}</span>
-          </h2>
-          <nav className="relative flex flex-wrap items-center justify-center w-full gap-14">
-            {filteredVideos.length > 0 ? (
-              filteredVideos.map(
-                ({ label, slug, product, thumbnail: poster, videoURL }) => (
-                  <VideoBlock
+    <>
+      <Head>
+        <title>{t('meta.title.search')}</title>
+        <meta name="description" content={t('meta.content.search')} />
+        <meta name="title" content={t('meta.title.search')} />
+        <meta
+          name="keywords"
+          content="Zaxe, Zaxe Knowledge Base, Zaxe 3D, Knowledge, 3D Printer, 3D Printing, Slicer, Filament"
+        />
+        <meta name="description" content={t('meta.content.search')} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://knowledge-base.zaxe.com/search"
+        />
+        <meta property="og:title" content={t('meta.title.search')} />
+        <meta property="og:description" content={t('meta.content.search')} />
+        <meta
+          property="og:keywords"
+          content="Zaxe, Zaxe Knowledge Base, Zaxe 3D, Knowledge, 3D Printer, 3D Printing, Slicer, Filament"
+        />
+        <meta property="og:image" content={Images.og.home.default.src} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://knowledge-base.zaxe.com/search"
+        />
+        <meta property="twitter:site" content="@Zaxe3D" />
+        <meta property="twitter:site:id" content="@Zaxe3D" />
+        <meta property="twitter:creator" content="@Zaxe3D" />
+        <meta property="twitter:creator:id" content="@Zaxe3D" />
+        <meta property="twitter:title" content={t('meta.title.search')} />
+        <meta
+          property="twitter:description"
+          content={t('meta.content.search')}
+        />
+        <meta property="twitter:image" content={Images.og.home.default.src} />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link
+          rel="alternate"
+          hrefLang="tr"
+          href="https://knowledge-base.zaxe.com/tr/search"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en"
+          href="https://knowledge-base.zaxe.com/search"
+        />
+      </Head>
+      <section className="pt-[20vh] font-zaxe w-full flex flex-wrap justify-center items-start">
+        <section className="grid w-full grid-cols-1 p-5 gap-14 max-w-app place-content-start place-items-center">
+          <SearchBar value={searchQuery || null} />
+          <section className="grid w-full grid-cols-1 pb-10 gap-7 place-content-start place-items-center">
+            <h2 className="flex items-center justify-center gap-2 p-1 px-4 text-xl font-semibold text-center text-white shadow-xl shadow-black/30 rounded-xl bg-zaxe">
+              <MdArticle />
+              <span>{t('common:search.articles')}</span>
+            </h2>
+            <nav className="relative flex flex-wrap items-center justify-center w-full gap-14">
+              {filteredPosts.length > 0 ? (
+                filteredPosts.map((post) => (
+                  <PostBlock
+                    key={`post-${post.id}`}
                     props={{
-                      label,
-                      poster,
-                      videoURL,
-                      productName: Content.products.find(
-                        ({ slug: pSlug }) => pSlug === product
-                      ).name,
+                      postSlug: post.slug,
+                      postCategory: post['category-slug'],
+                      postTitle: { [router.locale]: post.title },
+                      postThumbnail: post.thumbnail,
+                      queryProduct: post['product-slug'],
+                      queryPCategory: post['product-category'],
+                      Categories: Content.products.find(
+                        ({ slug }) => slug === post['product-slug']
+                      ).content.categories,
                     }}
-                    key={`video-${slug}`}
                   />
+                ))
+              ) : (
+                <NoContent />
+              )}
+            </nav>
+          </section>
+          <section className="grid w-full grid-cols-1 pb-10 gap-7 place-content-start place-items-center">
+            <h2 className="flex items-center justify-center gap-2 p-1 px-4 text-xl font-semibold text-center text-white shadow-xl shadow-black/30 rounded-xl bg-zaxe">
+              <MdVideoLibrary />
+              <span>{t('common:search.videos')}</span>
+            </h2>
+            <nav className="relative flex flex-wrap items-center justify-center w-full gap-14">
+              {filteredVideos.length > 0 ? (
+                filteredVideos.map(
+                  ({ label, slug, product, thumbnail: poster, videoURL }) => (
+                    <VideoBlock
+                      props={{
+                        label,
+                        poster,
+                        videoURL,
+                        productName: Content.products.find(
+                          ({ slug: pSlug }) => pSlug === product
+                        ).name,
+                      }}
+                      key={`video-${slug}`}
+                    />
+                  )
                 )
-              )
-            ) : (
+              ) : (
+                <NoContent />
+              )}
+            </nav>
+          </section>
+          <section className="grid w-full grid-cols-1 pb-10 gap-7 place-content-start place-items-center">
+            <h2 className="flex items-center justify-center gap-2 p-1 px-4 text-xl font-semibold text-center text-white shadow-xl shadow-black/30 rounded-xl bg-zaxe">
+              <BsFillFileEarmarkArrowDownFill />
+              <span>{t('common:search.downloads')}</span>
+            </h2>
+            <nav className="relative flex flex-wrap items-center justify-center w-full gap-14">
               <NoContent />
-            )}
-          </nav>
-        </section>
-        <section className="grid w-full grid-cols-1 pb-10 gap-7 place-content-start place-items-center">
-          <h2 className="flex items-center justify-center gap-2 p-1 px-4 text-xl font-semibold text-center text-white shadow-xl shadow-black/30 rounded-xl bg-zaxe">
-            <BsFillFileEarmarkArrowDownFill />
-            <span>{t('common:search.downloads')}</span>
-          </h2>
-          <nav className="relative flex flex-wrap items-center justify-center w-full gap-14">
-            <NoContent />
-          </nav>
+            </nav>
+          </section>
         </section>
       </section>
-    </section>
+    </>
   );
 }
 
