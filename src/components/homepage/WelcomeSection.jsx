f71bb099dc67736696c4e39fc73505e71e01446c
JAEGER <omer.kayalar0101@icloud.com>
2022-08-25 15:46:34 +0300

    remove brackets

--- src/components/homepage/WelcomeSection.jsx
+++ src/components/homepage/WelcomeSection.jsx
@@ -26,7 +26,7 @@ function WelcomeSection() {
           </p>
         </div>
         <div className="relative grid w-full grid-cols-1 place-content-center place-items-center">
-          <SearchBar className={'homepage-search-bar'} />
+          <SearchBar className="homepage-search-bar" />
         </div>
       </div>
     </div>
