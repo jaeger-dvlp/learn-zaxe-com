ad353b159d33bc20948cf1e9a543ef0b77fd0e67
JAEGER <omer.kayalar0101@icloud.com>
2022-09-02 16:46:23 +0300

    feat: get posts & videos

--- src/components/productpage/categories.jsx
+++ src/components/productpage/categories.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useTranslation } from 'next-i18next';
+import { i18n, useTranslation } from 'next-i18next';
+import { BiPrinter } from 'react-icons/bi';
+import { MdArticle, MdVideoLibrary } from 'react-icons/md';
+import Image from 'next/image';
+import { useAppContext } from '../contexts/AppContext';
 
 function Categories({ product }) {
   const { t } = useTranslation();
+  const [activeCategory, setActiveCategory] = React.useState(null);
   const router = useRouter();
+  const {
+    query: { product: queryProduct, category: queryPCategory },
+  } = router;
 
   return (
     <div
@@ -18,33 +27,294 @@ function Categories({ product }) {
         <h1 className="text-4xl font-bold text-zaxe">
           {t('product-page-components.categories.title')}
         </h1>
-        <div className="relative grid w-full max-w-md grid-cols-1 gap-5 xl:max-w-5xl lg:max-w-5xl xl:grid-cols-2 lg:grid-cols-2 gap-x-14 place-content-center xl:place-items-start lg:place-items-start place-items-center">
-          {product.content.categories.map(({ label, slug }) => (
-            <Link
-              key={slug}
-              href={`${router.asPath}/categories?c=${slug}`}
-              locale={router.locale}
+        <div className="relative flex items-start justify-center w-full xl:hidden lg:hidden">
+          <div className="grid w-full max-w-sm grid-cols-1 gap-3 place-content-start place-items-center">
+            <Link href={`${router.asPath}/categories`} locale={router.locale}>
+              <a className="flex items-center w-full gap-5 p-3 px-5 transition-all duration-150 bg-zaxe/10 text-zaxe hover:bg-zaxe/30 active:scale-90 rounded-xl">
+                <BiPrinter className="text-3xl" />
+                <span className="text-xl font-semibold text-center xl:text-left lg:text-left group-hover:font-semibold">
+                  {t('product-page-components.categories.all-categories')}
+                </span>
+              </a>
+            </Link>
+            {product.content.categories.map(({ label, slug }) => (
+              <Link
+                key={`category-route-${slug}`}
+                href={`${router.asPath}/categories?c=${slug}`}
+                className="flex items-center justify-start w-full gap-5 p-3 px-5 transition-all duration-150 bg-zaxe/10 text-zaxe hover:bg-zaxe/30 active:scale-90 rounded-xl"
+                locale={router.locale}
+              >
+                <a className="flex items-center justify-start w-full gap-5 p-3 px-5 transition-all duration-150 bg-zaxe/10 text-zaxe hover:bg-zaxe/30 active:scale-90 rounded-xl">
+                  <BiPrinter className="text-3xl" />
+                  <span className="text-xl font-semibold text-center xl:text-left lg:text-left group-hover:font-semibold">
+                    {t(label)}
+                  </span>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="relative items-start justify-center hidden w-full gap-10 xl:flex lg:flex">
+          <div className="w-full grid max-w-[17.5rem] grid-cols-1 gap-3 place-content-start place-items-start">
+            <button
+              onClick={() => setActiveCategory(null)}
+              className={`${
+                activeCategory === null
+                  ? 'bg-zaxe text-white hover:bg-zaxe'
+                  : 'bg-zaxe/10 text-zaxe hover:bg-zaxe/30'
+              } flex items-center active:scale-90 justify-start w-full gap-5 p-3 px-5 transition-all duration-150 rounded-xl `}
+              type="button"
             >
-              <a className="relative flex items-center justify-center w-full p-3 border-b-2 rounded-none xl:justify-between lg:justify-between group border-b-transparent hover:border-b-zaxe">
-                <span className="text-2xl font-normal text-center xl:text-left lg:text-left group-hover:font-semibold text-zaxe">
+              <BiPrinter className="text-3xl" />
+              <span className="text-xl font-semibold text-center xl:text-left lg:text-left group-hover:font-semibold">
+                {t('product-page-components.categories.all-categories')}
+              </span>
+            </button>
+            {product.content.categories.map(({ label, slug }) => (
+              <button
+                key={`category-button-${slug}`}
+                onClick={() => setActiveCategory(slug)}
+                className={`${
+                  slug === activeCategory
+                    ? 'bg-zaxe text-white hover:bg-zaxe'
+                    : 'bg-zaxe/10 text-zaxe hover:bg-zaxe/30'
+                } flex items-center active:scale-90 justify-start w-full gap-5 p-3 px-5 transition-all duration-150 rounded-xl `}
+                type="button"
+              >
+                <BiPrinter className="text-3xl" />
+                <span className="text-xl font-semibold text-center xl:text-left lg:text-left group-hover:font-semibold">
                   {t(label)}
                 </span>
-                <span className="text-xl xl:block lg:block hidden font-normal text-[#E8E8E8] group-hover:text-[#9A9A9A]">
-                  {t('product-page-components.categories.gotopage-button')}
-                </span>
-              </a>
-            </Link>
-          ))}
-          <div className="flex items-center justify-center w-full mt-14 col-span-full">
-            <Link href={`${router.asPath}/categories`} locale={router.locale}>
-              <a className="text-center hover:bg-zaxe hover:text-white transition-all duration-150 min-w-[155px] p-2 px-10 rounded-full bg-white shadow-xl border border-zinc-100 text-[#686868] text-xl font-medium">
-                {t('product-page-components.categories.see-all')}
-              </a>
-            </Link>
+              </button>
+            ))}
+          </div>
+          <div className="flex flex-wrap items-start justify-start w-full max-w-xl gap-10">
+            {activeCategory === null ? (
+              <>
+                {product.content.posts.map(
+                  ({
+                    slug: postSlug,
+                    category: postCategory,
+                    title: postTitle,
+                    thumbnail: postThumbnail,
+                  }) => (
+                    <PostBlock
+                      key={Math.floor(Math.random() * 100000)}
+                      props={{
+                        postSlug,
+                        postCategory,
+                        postTitle,
+                        postThumbnail,
+                        queryProduct,
+                        queryPCategory,
+                        Categories: product.content.categories,
+                      }}
+                    />
+                  )
+                )}
+                {product.content.starterVideos
+                  .slice(0, 2)
+                  .map(({ label, videoURL, thumbnail }) => (
+                    <VideoBlock
+                      key={Math.floor(Math.random() * 100000)}
+                      props={{
+                        label,
+                        videoURL,
+                        poster: thumbnail,
+                        productName: product.name,
+                      }}
+                    />
+                  ))}
+              </>
+            ) : (
+              <>
+                {product.content.posts
+                  .filter(({ category }) => category === activeCategory)
+                  .slice(0, 2)
+                  .map(
+                    ({
+                      slug: postSlug,
+                      category: postCategory,
+                      title: postTitle,
+                      thumbnail: postThumbnail,
+                    }) => (
+                      <PostBlock
+                        key={Math.floor(Math.random() * 100000)}
+                        props={{
+                          postSlug,
+                          postCategory,
+                          postTitle,
+                          postThumbnail,
+                          queryProduct,
+                          queryPCategory,
+                          Categories: product.content.categories,
+                        }}
+                      />
+                    )
+                  )}
+                {product.content.starterVideos
+                  .filter(({ category }) => category === activeCategory)
+                  .slice(0, 2)
+                  .map(
+                    ({ label, videoURL, thumbnail, product: videoProduct }) => (
+                      <VideoBlock
+                        key={Math.floor(Math.random() * 100000)}
+                        props={{
+                          label,
+                          videoURL,
+                          poster: thumbnail,
+                          productName: videoProduct,
+                        }}
+                      />
+                    )
+                  )}
+              </>
+            )}
+            <GetSeeAllButton props={{ activeCategory, product, router }} />
           </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function GetSeeAllButton({ props }) {
+  const { activeCategory, product, router } = props;
+
+  if (activeCategory) {
+    if (
+      product.content.starterVideos
+        .filter(({ category }) => category === activeCategory)
+        .slice(0, 2).length > 0 ||
+      product.content.posts
+        .filter(({ category }) => category === activeCategory)
+        .slice(0, 2).length > 0
+    ) {
+      return (
+        <SeeAllButton
+          key={Math.floor(Math.random() * 100000)}
+          props={{ activeCategory, router }}
+        />
+      );
+    }
+    return null;
+  }
+
+  return (
+    <SeeAllButton
+      key={Math.floor(Math.random() * 100000)}
+      props={{ activeCategory, router }}
+    />
+  );
+}
+
+function SeeAllButton({ props }) {
+  const { router, activeCategory } = props;
+  const { t } = useTranslation();
+  return (
+    <div className="flex justify-center w-full">
+      <Link
+        href={`${router.asPath}/categories${
+          activeCategory ? `?c=${activeCategory}` : ''
+        }`}
+        locale={router.locale}
+      >
+        <a className="text-center anim-fade-up-f hover:bg-zaxe hover:text-white transition-all duration-150 min-w-[155px] p-2 px-10 rounded-full bg-white shadow-xl border border-zinc-100 text-[#686868] text-xl font-medium">
+          {t('product-page-components.categories.see-all')}
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+function PostBlock({ props }) {
+  const { postSlug, postTitle, postThumbnail, queryProduct, queryPCategory } =
+    props;
+  const router = useRouter();
+  const { t } = useTranslation();
+
+  return (
+    <Link
+      href={`/products/${queryPCategory}/${queryProduct}/article/${postSlug}`}
+      locale={router.locale}
+    >
+      <a
+        className="text-black group hover:-translate-y-3 hover:bg-zinc-100 hover:border-zaxe/70 transition-all duration-200 anim-fade-up-f max-w-[16rem] w-full grid-cols-1 place-content-start
+      place-items-start bg-white shadow-lg border rounded-xl overflow-hidden"
+      >
+        <figure className="w-full rounded-b-xl overflow-hidden relative h-[10rem]">
+          <Image
+            src={postThumbnail}
+            layout="fill"
+            alt={postTitle.en}
+            className="object-cover z-[1] object-center w-full h-full"
+          />
+        </figure>
+        <div className="flex flex-wrap items-center justify-between w-full p-3 ">
+          <div className="w-[85%] grid grid-cols-1 place-content-start place-items-start">
+            <h2 className="text-sm font-semibold text-left transition-all duration-200 group-hover:text-zaxe text-zinc-600">
+              {postTitle[i18n.language]}
+            </h2>
+            <span className="text-xs text-left text-zinc-400">
+              {t('product-page-components.categories.article')}
+            </span>
+          </div>
+          <div className="flex items-center justify-end w-[10%] h-full">
+            <span className="p-1 z-[2]  rounded-md text-white bg-zaxe">
+              <MdArticle className="text-xl" />
+            </span>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+}
+
+function VideoBlock({ props }) {
+  const { label, videoURL, poster, productName } = props;
+  const { activateVideoPopup } = useAppContext();
+  const { t } = useTranslation();
+  const playVideo = () => {
+    activateVideoPopup({
+      video: {
+        title: `${t(label)} | ${productName}`,
+        url: videoURL,
+        poster,
+      },
+    });
+  };
+  return (
+    <button
+      className="text-black group hover:-translate-y-3 hover:bg-zinc-100 hover:border-zaxe/70 transition-all duration-200 anim-fade-up-f max-w-[16rem] w-full grid-cols-1 place-content-start
+      place-items-start bg-white shadow-lg border rounded-xl overflow-hidden"
+      type="button"
+      onClick={() => playVideo()}
+      onKeyPress={() => playVideo()}
+    >
+      <figure className="w-full rounded-b-xl overflow-hidden relative h-[10rem]">
+        <Image
+          src={poster}
+          layout="fill"
+          alt={label}
+          className="object-cover z-[1] object-center w-full h-full"
+        />
+      </figure>
+      <div className="flex items-center justify-between w-full p-3 ">
+        <div className="w-[85%] grid grid-cols-1 place-content-start place-items-start">
+          <h2 className="text-sm font-semibold text-left transition-all duration-200 group-hover:text-zaxe text-zinc-600">
+            {t(label)}
+          </h2>
+          <span className="text-xs text-left text-zinc-400">
+            {t('product-page-components.categories.video')}
+          </span>
+        </div>
+        <div className="flex items-center justify-end w-full h-full">
+          <span className="p-1 z-[2] rounded-md text-white bg-zaxe">
+            <MdVideoLibrary className="text-xl" />
+          </span>
+        </div>
+      </div>
+    </button>
   );
 }
 
