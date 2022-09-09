241fb9c9338a6ad1ea9fe0e0e6ca2f2ff92aeaa0
JAEGER <omer.kayalar0101@icloud.com>
2022-09-09 13:43:11 +0300

    feat: create Microdata component

---
+++ src/components/articles/Schema.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Images from '@/src/images/Images';
+import { useTranslation } from 'next-i18next';
+import Head from 'next/head';
+
+function Schema({ props }) {
+  const {
+    postTitle,
+    postSlug,
+    queryProductSlug,
+    queryCategorySlug,
+    productName,
+  } = props;
+  const { t } = useTranslation();
+
+  const getID = () => {
+    if (productName) {
+      return `https://knowledge-base.zaxe.com/products/${queryCategorySlug}/${queryProductSlug}/article/${postSlug}`;
+    }
+    return `https://knowledge-base.zaxe.com/articles/${postSlug}`;
+  };
+
+  const getHeadline = () => {
+    if (productName) {
+      return `${postTitle} - ${productName}${t('meta.title.article')}`;
+    }
+    return `${postTitle}${t('meta.title.article')}`;
+  };
+
+  return (
+    <Head>
+      <script type="application/ld+json">
+        {JSON.stringify({
+          '@context': 'http://schema.org',
+          '@type': 'Article',
+          mainEntityOfPage: {
+            '@type': 'WebPage',
+            '@id': getID(),
+          },
+          headline: getHeadline(),
+          image: {
+            '@type': 'ImageObject',
+            url: `https://knowledge-base.zaxe.com${Images.og.home.default.src}`,
+          },
+        })}
+      </script>
+    </Head>
+  );
+}
+
+export default Schema;
