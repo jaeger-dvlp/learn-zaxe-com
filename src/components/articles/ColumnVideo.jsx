2709673197b818cd846fb23978f6c38394c5e3f4
JAEGER <omer.kayalar0101@icloud.com>
2022-09-09 13:46:35 +0300

    rev: semantic html

--- src/components/articles/ColumnVideo.jsx
+++ src/components/articles/ColumnVideo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { v4 } from 'uuid';
-import VideoControls from './VideoControls';
+import VideoControls from '@/src/components/articles/VideoControls';
 
 function ColumnVideo({ children, video: { url: videoURL } }) {
   const [videoID, setVideoID] = React.useState(null);
@@ -11,25 +11,29 @@ function ColumnVideo({ children, video: { url: videoURL } }) {
 
   return (
     <section className="flex flex-wrap-reverse w-full gap-10 my-5 post-column xl:flex-nowrap lg:flex-nowrap">
-      <figure
-        className="relative bg-zinc-800 border border-zinc-300 p-0 w-full overflow-hidden shadow-2xl shadow-black/25
-      rounded-xl xl:h-[20rem] lg:h-[20rem] md:h-[17rem] h-[13rem]"
+      <section
+        className="relative bg-zinc-800 border border-zinc-300 p-0 w-full overflow-hidden shadow-2xl
+        shadow-black/25 rounded-xl xl:h-[20rem] lg:h-[20rem] md:h-[17rem] h-[13rem]"
       >
-        <VideoControls videoID={videoID} videoURL={videoURL} />
         <video
+          className="object-cover z-[3] w-full h-full"
           id={`video-${videoID}`}
+          preload="metadata"
+          type="video/mp4"
+          autoPlay={false}
+          src={videoURL}
+          height="100%"
+          width="100%"
           controls
           muted
-          autoPlay={false}
-          className="object-cover z-[3] w-full h-full"
         >
-          <track kind="captions" />
-          <source src={videoURL} />
+          <source src={videoURL} type="video/mp4" />
         </video>
-      </figure>
-      <article className="w-full -order-1 xl:max-w-[40%] lg:max-w-[40%] max-w-full">
+        <VideoControls videoID={videoID} videoURL={videoURL} />
+      </section>
+      <section className="w-full -order-1 xl:max-w-[40%] lg:max-w-[40%] max-w-full">
         {children}
-      </article>
+      </section>
     </section>
   );
 }
