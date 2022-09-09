db6e41526c577e65407126a30cb52de9aacc0e3c
JAEGER <omer.kayalar0101@icloud.com>
2022-09-09 16:40:24 +0300

    rev: more semantic html friendly

--- src/components/articles/Breadcrumbs.jsx
+++ src/components/articles/Breadcrumbs.jsx
@@ -10,34 +10,32 @@ function Breadcrumbs({ links }) {
   const { t } = useTranslation();
 
   return (
-    <section
+    <nav
       id="bread"
       className="flex flex-wrap items-start justify-center w-full p-5 py-0"
     >
-      <nav className="w-full">
-        <ul className="flex flex-wrap items-center justify-center w-full gap-2 text-xs font-medium text-zinc-400">
-          <li>
-            <Link href="/" locale={locale}>
-              <a className="hover:underline">
-                {t('common:bread-crumbs.knowledge-base')}
-              </a>
-            </Link>
-          </li>
-          {links &&
-            links.map(({ text: urlText, url }) => (
-              <li
-                key={`bread-crumb-${urlText}`}
-                className="flex items-center justify-center gap-2"
-              >
-                <BsChevronRight />
-                <Link href={url}>
-                  <a className="hover:underline">{urlText}</a>
-                </Link>
-              </li>
-            ))}
-        </ul>
-      </nav>
-    </section>
+      <ul className="flex flex-wrap items-center justify-center w-full gap-2 text-xs font-medium text-zinc-400">
+        <li>
+          <Link href="/" locale={locale}>
+            <a className="hover:underline">
+              {t('common:bread-crumbs.knowledge-base')}
+            </a>
+          </Link>
+        </li>
+        {links &&
+          links.map(({ text: urlText, url }) => (
+            <li
+              key={`bread-crumb-${urlText}`}
+              className="flex items-center justify-center gap-2"
+            >
+              <BsChevronRight />
+              <Link href={url}>
+                <a className="hover:underline">{urlText}</a>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </nav>
   );
 }
 
