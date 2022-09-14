19d446b6ff245ef12d9503debe8190857025ed81
JAEGER <omer.kayalar0101@icloud.com>
2022-09-14 17:09:41 +0300

    feat: handle global posts too

--- src/components/misc/PostBlock.jsx
+++ src/components/misc/PostBlock.jsx
@@ -8,6 +8,7 @@ import { i18n, useTranslation } from 'next-i18next';
 
 function PostBlock({ props }) {
   const {
+    type,
     postSlug,
     postCategory,
     postTitle,
@@ -20,17 +21,42 @@ function PostBlock({ props }) {
   const router = useRouter();
   const { locale } = router;
   const Product = Content.products.find(({ slug }) => queryProduct === slug);
+
+  const getPostLink = () => {
+    if (type === 'product') {
+      return `/products/${queryPCategory}/${queryProduct}/article/${postSlug}`;
+    }
+    return `/articles/${postSlug}`;
+  };
+
+  const getPostProduct = () => {
+    if (type === 'product') {
+      return (
+        <span className="absolute bottom-0 z-[3] m-3 p-1 px-2 text-white rounded-md bg-zaxe shadow-md text-xs">
+          {Product.model}
+        </span>
+      );
+    }
+    return null;
+  };
+
+  const getPostCategory = () => {
+    if (type === 'product') {
+      if (Categories.find(({ slug }) => slug === postCategory.slug)) {
+        return t(
+          Categories.find(({ slug }) => slug === postCategory.slug).label
+        );
+      }
+    }
+    return t('content-global:categories.0.label');
+  };
+
   return (
-    <Link
-      href={`/products/${queryPCategory}/${queryProduct}/article/${postSlug}`}
-      locale={locale}
-    >
+    <Link href={getPostLink()} locale={locale}>
       <a className="relative grid w-full h-full max-w-[16rem] grid-cols-1 overflow-hidden transition-all duration-200 bg-white shadow-xl cursor-pointer hover:-translate-y-3 hover:shadow-2xl place-content-start place-items-center rounded-xl">
         <BiLinkExternal className="absolute z-[3] text-4xl bottom-2 right-2 text-zinc-200" />
         <figure className="relative z-[2] w-full h-[10rem] max-w-xs overflow-hidden rounded-xl">
-          <span className="absolute bottom-0 z-[3] m-3 p-1 px-2 text-white rounded-md bg-zaxe shadow-md text-xs">
-            {Product.model}
-          </span>
+          {getPostProduct()}
           <Image
             src={postThumbnail}
             layout="fill"
@@ -40,14 +66,7 @@ function PostBlock({ props }) {
         </figure>
         <section className="relative z-[1] flex items-end justify-between w-full p-3 bg-white">
           <section className="grid w-[80%] gap-1 grid-cols-1 place-content-end place-items-start">
-            <span className="text-xs text-[#868686]">
-              {(Categories.find(({ slug }) => slug === postCategory.slug) &&
-                t(
-                  Categories.find(({ slug }) => slug === postCategory.slug)
-                    .label
-                )) ||
-                null}
-            </span>
+            <span className="text-xs text-[#868686]">{getPostCategory()}</span>
             <h2 className="text-sm post-name text-[#555555]">
               {postTitle[i18n.language]}
             </h2>
