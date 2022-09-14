ffdc7c3f4f6e6e246e0954858acd8d02060bf1bb
JAEGER <omer.kayalar0101@icloud.com>
2022-09-14 11:03:13 +0300

    feat: Create Markdown Aligned Component

---
+++ src/components/articles/Aligned.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Aligned({ children }) {
+  return (
+    <span className="grid w-full grid-cols-1 place-content-start place-items-center">
+      <section className="w-full max-w-4xl text-justify">{children}</section>
+    </span>
+  );
+}
+
+export default Aligned;
