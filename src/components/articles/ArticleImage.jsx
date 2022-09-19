3998455ce0ebc43dde5497dbb39d23d1331273a7
JAEGER <omer.kayalar0101@icloud.com>
2022-09-19 15:56:45 +0300

    feat: Create component

---
+++ src/components/articles/ArticleImage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/future/image';
+import ImageControls from './ImageControls';
+
+function ArticleImage({ className, image: { imageURL, imageALT } }) {
+  const { CDNURL } = process.env;
+  const ImageSourceURL = imageURL.includes('http')
+    ? imageURL
+    : `${CDNURL}${imageURL}`;
+  return (
+    <figure
+      className={`${className} bg-zinc-300 relative border border-zinc-300 p-0 w-full overflow-hidden shadow-lg rounded-xl xl:h-[20rem] lg:h-[20rem] md:h-[17rem] h-[13rem]`}
+    >
+      <Image
+        src={ImageSourceURL}
+        alt={imageALT}
+        layout="fill"
+        className="object-cover p-0 !border-none object-center w-full h-full"
+      />
+      <ImageControls props={{ type: 'single', imageURL }} />
+    </figure>
+  );
+}
+
+export default ArticleImage;
