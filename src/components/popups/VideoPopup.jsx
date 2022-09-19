14c30b27221f77a5a1c24c447a651ec6319da665
JAEGER <omer.kayalar0101@icloud.com>
2022-09-19 16:57:23 +0300

    fix: html

--- src/components/popups/VideoPopup.jsx
+++ src/components/popups/VideoPopup.jsx
@@ -22,9 +22,9 @@ function VideoPopup() {
       >
         <div className="relative w-full p-3 bg-white shadow-xl max-w-app rounded-xl">
           <div className="absolute left-0 flex flex-wrap items-center justify-between w-full gap-2 p-0 m-0 -translate-y-full -top-1">
-            <h1 className="p-2 px-3 font-semibold bg-white rounded-lg text-zaxe">
+            <h2 className="p-2 px-3 font-semibold bg-white rounded-lg text-zaxe">
               {videoTitle}
-            </h1>
+            </h2>
             <div className="flex items-center justify-center gap-2 p-0 min-w-fit">
               <a
                 className="flex items-center justify-center w-10 h-10 p-2 text-3xl text-center transition-all duration-200 bg-white rounded-lg hover:bg-zaxe hover:text-white text-zaxe"
