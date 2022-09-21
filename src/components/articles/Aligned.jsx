5a3070ab34d2c6bdfeb0ae1463867e50ca43bedf
JAEGER <omer.kayalar0101@icloud.com>
2022-09-21 14:47:21 +0300

    rev: classname of section

--- src/components/articles/Aligned.jsx
+++ src/components/articles/Aligned.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Aligned({ children }) {
   return (
-    <span className="grid w-full grid-cols-1 place-content-start place-items-center">
+    <span className="grid w-full grid-cols-1 aligned place-content-start place-items-center">
       <section className="w-full max-w-4xl text-justify">{children}</section>
     </span>
   );
