db8f280bd7b64c5594dbc799a1e34132e90160f9
JAEGER <omer.kayalar0101@icloud.com>
2022-09-26 08:51:31 +0300

    feat: render if videos exists

--- src/components/productpage/getStarted.jsx
+++ src/components/productpage/getStarted.jsx
@@ -8,27 +8,29 @@ function GetStarted({ product }) {
   const { name } = product;
 
   return (
-    <div
-      data-aos="fade"
-      data-aos-delay={300}
-      data-aos-duration={500}
-      className="grid w-full grid-cols-1 p-0 m-0 bg-white place-content-start place-items-center"
-    >
-      <div className="grid w-full grid-cols-1 gap-20 p-5 py-20 max-w-app place-content-start place-items-center">
-        <h1 className="text-4xl font-bold text-zaxe">
-          {t('product-page-components.get-started.title')}
-        </h1>
-        <div className="flex flex-wrap items-center justify-center w-full gap-16">
-          {starterVideos &&
-            starterVideos.map(({ label, thumbnail: poster, videoURL }) => (
-              <VideoBlock
-                key={`${label}Video`}
-                props={{ label, poster, videoURL, productName: name }}
-              />
-            ))}
+    starterVideos.length > 0 && (
+      <div
+        data-aos="fade"
+        data-aos-delay={300}
+        data-aos-duration={500}
+        className="grid w-full grid-cols-1 p-0 m-0 bg-white place-content-start place-items-center"
+      >
+        <div className="grid w-full grid-cols-1 gap-20 p-5 py-20 max-w-app place-content-start place-items-center">
+          <h1 className="text-4xl font-bold text-zaxe">
+            {t('product-page-components.get-started.title')}
+          </h1>
+          <div className="flex flex-wrap items-center justify-center w-full gap-16">
+            {starterVideos &&
+              starterVideos.map(({ label, thumbnail: poster, videoURL }) => (
+                <VideoBlock
+                  key={`${label}Video`}
+                  props={{ label, poster, videoURL, productName: name }}
+                />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
